d5f0cc26a86de07d215ac31b74727ace672d89f8
Mikołaj <mikolaj111izdebski@gmail.com>
2026-09-14 18:41:12 +0200

    Zadanie 2

--- projekt/src/Main1.java
+++ projekt/src/Main1.java
@@ -3,5 +3,9 @@ public class Main1 {
         System.out.println("Ania");
         System.out.println("Bartek");
         System.out.println("Kasia");
+
+        String imie = "Mikołaj";
+        int rokUrodzenia = 2007;
+        double liczba = 0.66;
     }
 }
