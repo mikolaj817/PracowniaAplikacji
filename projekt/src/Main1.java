cab728fd96eb5b06454c9a9cf8c73052e662169f
Mikołaj <mikolaj111izdebski@gmail.com>
2026-09-14 18:49:53 +0200

    Zadanie 4

--- projekt/src/Main1.java
+++ projekt/src/Main1.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Main1 {
+
+
     public static void main(String[] args) {
+
         System.out.println("Ania");
         System.out.println("Bartek");
         System.out.println("Kasia");
@@ -8,5 +12,15 @@ public class Main1 {
         int rokUrodzenia = 2007;
         double liczba = 0.66;
         System.out.println("Mam na imię " + imie + ", mam " + (2026 - rokUrodzenia) + " lat i będę pisać maturę za " + liczba + " roku.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj temperaturę w stopniach Celsjusza:");
+        double stopnie = scanner.nextDouble();
+
+        double fahrenheit = 1.8 * stopnie + 32.0;
+
+        System.out.println("Temperatura w Fahrenheitach: " + fahrenheit);
     }
+
 }
