c20ff8765c2223d27ab1bb62f799c58eeb8e1ac2
Mikołaj <mikolaj111izdebski@gmail.com>
2026-09-17 19:27:36 +0200

    Zadanie 5

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -62,5 +62,50 @@ public class Main {
         }
 
         System.out.println("Największa liczba: " + najwieksza);
+
+
+        System.out.println("Podaj numer miesiąca:");
+        int miesiac = scanner.nextInt();
+
+        switch (miesiac) {
+            case 1:
+                System.out.println("Styczeń");
+                break;
+            case 2:
+                System.out.println("Luty");
+                break;
+            case 3:
+                System.out.println("Marzec");
+                break;
+            case 4:
+                System.out.println("Kwiecień");
+                break;
+            case 5:
+                System.out.println("Maj");
+                break;
+            case 6:
+                System.out.println("Czerwiec");
+                break;
+            case 7:
+                System.out.println("Lipiec");
+                break;
+            case 8:
+                System.out.println("Sierpień");
+                break;
+            case 9:
+                System.out.println("Wrzesień");
+                break;
+            case 10:
+                System.out.println("Październik");
+                break;
+            case 11:
+                System.out.println("Listopad");
+                break;
+            case 12:
+                System.out.println("Grudzień");
+                break;
+            default:
+                System.out.println("Nieprawidlowy numer miesiaca");
+        }
     }
 }
