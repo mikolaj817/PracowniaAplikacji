532b8ac6931e1c8b2edbd4bd70d7029ea3e06435
Mikołaj <mikolaj111izdebski@gmail.com>
2026-09-17 19:39:22 +0200

    Zadanie 8

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -126,6 +126,7 @@ public class Main {
             System.out.println("Nie, masz inne imię.");
         }
         */
+        /*
         // Zadanie 7
         System.out.println("Podaj wiek:");
         int wiek = scanner.nextInt();
@@ -133,5 +134,15 @@ public class Main {
         boolean pelnoletni = wiek >= 18 ? true : false;
 
         System.out.println(pelnoletni);
+         */
+        // Zadanie 8
+        System.out.println("Podaj rok:");
+        int rok = scanner.nextInt();
+
+        if ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0) {
+            System.out.println("Rok jest przestępny.");
+        } else {
+            System.out.println("Rok nie jest przestępny.");
+        }
     }
 }
