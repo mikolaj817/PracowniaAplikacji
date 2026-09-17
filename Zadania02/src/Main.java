adf34db92b692f6f0f0dee97a8f77811f7765d99
Mikołaj <mikolaj111izdebski@gmail.com>
2026-09-17 19:20:40 +0200

    Zadanie 2

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -12,5 +12,21 @@ public class Main {
         } else {
             System.out.println("Liczba nie jest podzielna przez 3.");
         }
+
+
+        System.out.println("Podaj pierwszy bok:");
+        int bok1 = scanner.nextInt();
+
+        System.out.println("Podaj drugi bok:");
+        int bok2 = scanner.nextInt();
+
+        System.out.println("Podaj trzeci bok:");
+        int bok3 = scanner.nextInt();
+
+        if (bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1) {
+            System.out.println("Można zbudować trójkąt.");
+        } else {
+            System.out.println("Nie można zbudować trójkąta.");
+        }
     }
 }
